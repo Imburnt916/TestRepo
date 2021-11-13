587e6a89beb51f040c429d343799aff18a73ecbd
Burnt <drr2285@gmail.com>
2021-11-12 19:20:02 -0800

    Functionality completed and tested

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Ry Ry!");
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Hi");
+        }
     }
 }
